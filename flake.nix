a4ecba697c2770337c8c1f3213df078bf2a796d3
Ash Ametrine <ashu9s@proton.me>
2026-03-02 17:23:17 +0000

    chore: update flake and project metadata

--- flake.nix
+++ flake.nix
@@ -2,21 +2,39 @@
   description = "zgsld-agetty flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { nixpkgs, ... }: let
-    inherit (nixpkgs) lib;
-    forAllSystems = lib.genAttrs lib.systems.flakeExposed;
-  in {
-    devShells = forAllSystems(system: 
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in {
-        default = pkgs.mkShell {
-          name = "zgsld-agetty-devshell";
-          packages = with pkgs; [ zig zls linux-pam ];
-        };
-      });
-  };
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    in
+    {
+      formatter = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.nixfmt-rfc-style
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            name = "zgsld-agetty-devshell";
+            packages = with pkgs; [
+              zig
+              zls
+              linux-pam
+            ];
+          };
+        }
+      );
+    };
 }
